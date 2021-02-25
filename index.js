c56fe2b0d41bee14e2038c605b0b2ec15f4e6030
JessP070 <jcpereira070@gmail.com>
2021-02-24 18:54:31 -0800

    adding get request

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/products"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getProds()
+})
+
+function getProds(){
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(prods => {
+        prods.data.forEach(product => {
+          
+            const productMarkup = `
+              <div data-id=${product.id}>
+                <img src=${product.attributes.image_url} height="200" width="250">
+                <h3>${product.attributes.name}</h3>
+                <p>${product.attributes.cart.total}</p>
+                <button data-id=${product.id}>edit</button>
+              </div>
+              <br><br>`;
+    
+              document.querySelector('#product-container').innerHTML += productMarkup
+          })
+        })        
+}
