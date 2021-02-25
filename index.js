cf32a9a3c75c9db40864126bf1f3c553f7084d2c
JessP070 <jcpereira070@gmail.com>
2021-02-24 20:44:33 -0800

    create product form and prevent default

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const endPoint = "http://localhost:3000/api/v1/products"
 
 document.addEventListener('DOMContentLoaded', () => {
     getProds()
+
+    const createProductForm = document.querySelector("#create-product-form")
+
+    createProductForm.addEventListener("submit",(e) => createFormHandler(e))
+
 })
 
 function getProds(){
@@ -23,3 +28,20 @@ function getProds(){
           })
         })        
 }
+
+function createFormHandler(e) {
+
+e.preventDefault()
+    const nameInput = document.querySelector('#input-name').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const imageInput = document.querySelector('#image-name').value
+    const cartId = parseInt(document.querySelector('#cart').value)
+    postFetch(nameInput, descriptionInput, imageInput, cartId)
+}
+
+
+function postFetch(name, description, image_url, cart_id) {
+    console.log(name, description, image_url, cart_id);
+
+}
+
